7050a7c28eaf288c086fe56a6d7759b8cb5acbb9
duynghia <voduyanqn1972@gmail.com>
2024-07-13 15:55:09 +0700

    cast dim product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,5 @@
 SELECT 
-  stock_item_id AS product_key
-  , stock_item_name AS product_name
-  , brand AS brand_name
+  CAST(stock_item_id AS int) AS product_key
+  , CAST(stock_item_name AS STRING) AS product_name
+  , CAST(brand AS STRING) AS brand_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
