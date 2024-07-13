4af367bd1c45160797eadab839960a6ed76d5684
duynghia <voduyanqn1972@gmail.com>
2024-07-13 17:14:10 +0700

    cte fact sale order line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,34 @@
-SELECT 
-  cast(order_line_id as INT) as sales_order_line_key
-  , CAST(stock_item_id AS INT) as product_key
-  , CAST(quantity as INT) AS quantity
-  , CAST(unit_price AS NUMERIC) AS unit_price
-  , CAST(quantity AS INT) * CAST(unit_price AS NUMERIC) as gross_amount
+
+WITH sales_order_line__source AS (
+SELECT
+  *
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+
+, sales_order_line__rename_column AS (
+SELECT
+  order_line_id AS sales_order_line_key
+  , stock_item_id AS product_key
+  , quantity  AS quantity
+  , unit_price AS unit_price
+  , quantity * unit_price as gross_amount
+FROM sales_order_line__source
+)
+
+, sales_order_line__cast_type AS (
+SELECT
+  CAST(sales_order_line_key as INTEGER) as sales_order_line_key
+  , CAST(product_key AS INTEGER) as product_key
+  , CAST(quantity as INTEGER) AS quantity
+  , CAST(unit_price AS NUMERIC) AS unit_price
+  , CAST(quantity AS INTEGER) * CAST(unit_price AS NUMERIC) as gross_amount
+FROM sales_order_line__rename_column
+)
+
+SELECT 
+  sales_order_line_key
+  , product_key
+  , quantity
+  , unit_price
+  , gross_amount
+FROM sales_order_line__cast_type
