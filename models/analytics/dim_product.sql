862234ae0bb744f5cc9b1b7a81a7b8426c470568
duynghia <voduyanqn1972@gmail.com>
2024-07-13 16:12:47 +0700

    cte dim product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,30 @@
+
+
+
+WITH dim_product__source AS (
+  SELECT
+    *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+, dim_product__rename_column AS (
 SELECT 
-  CAST(stock_item_id AS int) AS product_key
-  , CAST(stock_item_name AS STRING) AS product_name
-  , CAST(brand AS STRING) AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  stock_item_id AS product_key
+  , stock_item_name AS product_name
+  , brand AS brand_name
+FROM dim_product__source
+)
+
+, dim_product__cast_type AS (
+SELECT 
+  CAST(product_key AS INTEGER) AS product_key
+  , CAST(product_name AS STRING) AS product_name
+  , CAST(brand_name AS STRING) AS brand_name
+FROM dim_product__rename_column
+)
+
+SELECT 
+  product_key
+  , product_name
+  , brand_name
+FROM dim_product__cast_type
