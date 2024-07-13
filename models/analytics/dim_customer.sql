a86d0c24d8ab01e9c87f6142c4a1586152607f1b
duynghia <voduyanqn1972@gmail.com>
2024-07-13 17:39:38 +0700

    cte dim customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,25 @@
+
+WITH dim_customer__source AS (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename_column AS (
+SELECT
+  customer_id AS customer_key
+  , customer_name 
+FROM dim_customer__source
+)
+
+, dim_customer__cast_type AS (
+SELECT
+  CAST(customer_key as INTEGER) as customer_key
+  , CAST(customer_name AS STRING) as customer_name
+FROM dim_customer__rename_column
+)
+
+SELECT 
+  customer_key
+  , customer_name
+FROM dim_customer__cast_type
