6098befe15eaf7aae82dbe921991b06a495467d8
duynghia <voduyanqn1972@gmail.com>
2024-07-14 20:16:38 +0700

    cte fact add customer

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -8,6 +8,7 @@ FROM `vit-lam-data.wide_world_importers.sales__order_lines`
 , sales_order_line__rename_column AS (
 SELECT
   order_line_id AS sales_order_line_key
+  , sales_order_id AS sales_order_key
   , stock_item_id AS product_key
   , quantity  AS quantity
   , unit_price AS unit_price
@@ -18,6 +19,7 @@ FROM sales_order_line__source
 , sales_order_line__cast_type AS (
 SELECT
   CAST(sales_order_line_key as INTEGER) as sales_order_line_key
+  , CAST(sales_order_key AS INTEGER) as sales_order_key
   , CAST(product_key AS INTEGER) as product_key
   , CAST(quantity as INTEGER) AS quantity
   , CAST(unit_price AS NUMERIC) AS unit_price
@@ -34,6 +36,7 @@ FROM sales_order_line__rename_column
 
 SELECT 
   sales_order_line_key
+  , sales_order_key
   , product_key
   , quantity
   , unit_price
