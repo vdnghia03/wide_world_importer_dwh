df2f33b0122e730f5301a58a606438134199969d
duynghia <voduyanqn1972@gmail.com>
2024-07-15 14:14:50 +0700

    dim supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,24 @@
+WITH supplier__source AS (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, supplier__rename_column AS (
+SELECT
+  supplier_id AS supplier_key
+  , supplier_name AS supplier_name
+FROM supplier__source
+)
+
+, supplier__cast_type AS (
+SELECT
+  CAST(supplier_key AS INTEGER) AS supplier_key
+  , CAST(supplier_name AS STRING) AS supplier_name
+FROM supplier__rename_column
+)
+
+SELECT
+  supplier_key
+  , supplier_name
+FROM supplier__cast_type
