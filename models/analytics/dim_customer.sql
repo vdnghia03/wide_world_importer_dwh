fd45f586c57bbbd0a4fed5ca32ca8f4f5d9e2b61
duynghia <voduyanqn1972@gmail.com>
2024-07-18 18:16:10 +0700

    update customer

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,4 +1,3 @@
-
 WITH dim_customer__source AS (
 SELECT 
   *
@@ -8,6 +7,8 @@ FROM `vit-lam-data.wide_world_importers.sales__customers`
 , dim_customer__rename_column AS (
 SELECT
   customer_id AS customer_key
+  , customer_catagory_id AS customer_category_key
+  , buying_group_id AS buying_group_key
   , customer_name 
 FROM dim_customer__source
 )
@@ -15,11 +16,21 @@ FROM dim_customer__source
 , dim_customer__cast_type AS (
 SELECT
   CAST(customer_key as INTEGER) as customer_key
+  , CAST(customer_category_key as INTEGER) as customer_category_key
+  , CAST(buying_group_key as INTEGER) as buying_group_key
   , CAST(customer_name AS STRING) as customer_name
 FROM dim_customer__rename_column
 )
 
 SELECT 
-  customer_key
-  , customer_name
-FROM dim_customer__cast_type
+  dim_customer.customer_key
+  , dim_customer.customer_category_key
+  , dim_customer.buying_group_key
+  , dim_customer.customer_name
+  , dim_customer_category.customer_category_name
+  , dim_buying_group.buying_group_name
+FROM dim_customer__cast_type AS dim_customer
+LEFT JOIN {{ ref('stg_dim_sales_customer_category') }} AS dim_customer_category
+ON dim_customer_category.customer_category_key = dim_customer__cast_type.customer_category_key
+LEFT JOIN {{ ref('stg_dim_sales_buying_group') }} AS dim_buying_group
+ON dim_buying_group.buying_group_key = dim_customer__cast_type.buying_group_key
