c9c92fcc709c7823a2a719b1d2c884c008e6d0c9
duynghia <voduyanqn1972@gmail.com>
2024-07-24 17:54:41 +0700

    update boolean

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,6 +9,7 @@ SELECT
   stock_item_id AS product_key
   , stock_item_name AS product_name
   , brand AS brand_name
+  , is_chiller_stock AS is_chiller_stock_boolean
   , supplier_id AS supplier_key
 FROM dim_product__source
 )
@@ -18,16 +19,30 @@ SELECT
   CAST(product_key AS INTEGER) AS product_key
   , CAST(product_name AS STRING) AS product_name
   , CAST(brand_name AS STRING) AS brand_name
+  , CAST(is_chiller_stock_boolean AS BOOLEAN) AS is_chiller_stock_boolean
   , CAST(supplier_key AS INTEGER) AS supplier_key
 FROM dim_product__rename_column
+)
+
+, dim_product__convert_boolean AS (
+  SELECT
+    *
+    , CASE 
+        WHEN is_chiller_stock_boolean = TRUE THEN 'Chiller Stock'
+        WHEN is_chiller_stock_boolean = FALSE THEN 'Not Chiller Stock'
+        WHEN is_chiller_stock_boolean IS NULL THEN 'Undefined'
+        ELSE 'Invalid'
+      END AS is_chiller_stock
+  FROM dim_product__cast_type
 )
 
 SELECT 
   dim_product.product_key
   , dim_product.product_name
   , dim_product.brand_name
+  , dim_product.is_chiller_stock
   , dim_product.supplier_key
   , dim_supplier.supplier_name
-FROM dim_product__cast_type AS dim_product
+FROM dim_product__convert_boolean AS dim_product
 LEFT JOIN {{ ref('dim_supplier') }} AS dim_supplier
 ON dim_product.supplier_key = dim_supplier.supplier_key
