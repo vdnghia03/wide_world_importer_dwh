35122cd4f8c96c3e8c58d5562f8b4ebc9cff4ddc
duynghia <voduyanqn1972@gmail.com>
2024-07-24 23:51:56 +0700

    update

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -39,7 +39,7 @@ FROM dim_product__rename_column
 SELECT 
   dim_product.product_key
   , dim_product.product_name
-  , dim_product.brand_name
+  , COALESCE(dim_product.brand_name, "Undefined") AS brand_name
   , dim_product.is_chiller_stock
   , dim_product.supplier_key
   , dim_supplier.supplier_name
