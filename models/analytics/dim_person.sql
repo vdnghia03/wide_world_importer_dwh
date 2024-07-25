b8969fc943f179955d3f989a9aeaa29a8bf63b5d
duynghia <voduyanqn1972@gmail.com>
2024-07-25 21:23:48 +0700

    update

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,26 @@
+
+WITH dim_person__source AS (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.application__people`
+)
+
+, dim_person__rename_column AS (
+SELECT
+  person_id AS person_key
+  , full_name
+FROM dim_person__source
+)
+
+, dim_person__cast_type AS (
+SELECT
+  CAST(person_key AS INT) AS person_key
+  , CAST(full_name AS STRING) AS full_name
+FROM dim_person__rename_column
+)
+
+
+SELECT
+  person_key
+  , full_name
+FROM dim_person__cast_type
