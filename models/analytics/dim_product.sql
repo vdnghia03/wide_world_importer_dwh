8693bff4bf7cf9092ad601272a47825be9a0461b
duynghia <voduyanqn1972@gmail.com>
2024-09-05 16:43:48 +0700

    update

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,7 +9,14 @@ WITH dim_product__source AS (
     stock_item_id AS product_key
     , stock_item_name AS product_name
     , brand AS brand_name
+    , size AS size_name
     , is_chiller_stock AS is_chiller_stock_boolean
+    , unit_price AS unit_price
+    , recommended_retail_price AS recommended_retail_price
+    , typical_weight_per_unit AS typical_weight_per_unit
+    , tax_rate AS tax_rate
+    , quantity_per_outer AS quantity_per_outer
+    , lead_time_days AS lead_time_days
     , supplier_id AS supplier_key
   FROM dim_product__source
 )
@@ -19,7 +26,14 @@ WITH dim_product__source AS (
     CAST(product_key AS INTEGER) AS product_key
     , CAST(product_name AS STRING) AS product_name
     , CAST(brand_name AS STRING) AS brand_name
+    , CAST(size_name AS STRING) AS size_name
     , CAST(is_chiller_stock_boolean AS BOOLEAN) AS is_chiller_stock_boolean
+    , CAST(unit_price AS FLOAT64) AS unit_price
+    , CAST(recommended_retail_price AS FLOAT64) AS recommended_retail_price
+    , CAST(typical_weight_per_unit AS FLOAT64) AS typical_weight_per_unit
+    , CAST(tax_rate AS FLOAT64) AS tax_rate
+    , CAST(quantity_per_outer AS INTEGER) AS quantity_per_outer
+    , CAST(lead_time_days AS INTEGER) AS lead_time_days
     , CAST(supplier_key AS INTEGER) AS supplier_key
   FROM dim_product__rename_column
 )
@@ -33,7 +47,25 @@ WITH dim_product__source AS (
         WHEN is_chiller_stock_boolean IS NULL THEN 'Undefined'
         ELSE 'Invalid'
       END AS is_chiller_stock
+
   FROM dim_product__cast_type
+)
+
+, dim_product_coalesce_record AS (
+  SELECT
+    product_key
+    , COALESCE(product_name, "Undefined") AS product_name
+    , COALESCE(brand_name, "Undefined") AS brand_name
+    , COALESCE(size_name, "Undefined") AS size_name
+    , is_chiller_stock
+    , unit_price
+    , recommended_retail_price
+    , typical_weight_per_unit
+    , tax_rate
+    , quantity_per_outer
+    , lead_time_days
+    , COALESCE(supplier_key, 0) AS supplier_key
+  FROM dim_product__convert_boolean
 )
 
 , dim_product__undefine_column AS (
@@ -41,16 +73,30 @@ WITH dim_product__source AS (
     product_key
     , product_name
     , brand_name
+    , size_name
     , is_chiller_stock
+    , unit_price
+    , recommended_retail_price
+    , typical_weight_per_unit
+    , tax_rate
+    , quantity_per_outer
+    , lead_time_days
     , supplier_key
-  FROM dim_product__convert_boolean
+  FROM dim_product__coalesce_record
 
   UNION ALL 
   SELECT
     0 as product_key
     , "Undefined" as product_name
     , "Undefined" as brand_name
+    , "Undefined" as size_name
     , "Undefined" as is_chiller_stock
+    , NULL as unit_price
+    , NULL as recommended_retail_price
+    , NULL as typical_weight_per_unit
+    , NULL as tax_rate
+    , NULL as quantity_per_outer
+    , NULL as lead_time_days
     , 0 as supplier_key
 
   UNION ALL
@@ -58,15 +104,29 @@ WITH dim_product__source AS (
     -1 as product_key
     , "Invalid" as product_name
     , "Invalid" as brand_name
+    , "Invalid" as size_name
     , "Invalid" as is_chiller_stock
+    , NULL as unit_price
+    , NULL as recommended_retail_price
+    , NULL as typical_weight_per_unit
+    , NULL as tax_rate
+    , NULL as quantity_per_outer
+    , NULL as lead_time_days
     , -1 as supplier_key
 )
 
 SELECT 
   dim_product.product_key
   , dim_product.product_name
-  , COALESCE(dim_product.brand_name, "Undefined") AS brand_name
+  , dim_product.brand_name
+  , dim_product.size_name
   , dim_product.is_chiller_stock
+  , dim_product.unit_price
+  , dim_product.recommended_retail_price
+  , dim_product.typical_weight_per_unit
+  , dim_product.tax_rate
+  , dim_product.quantity_per_outer
+  , dim_product.lead_time_days
   , dim_product.supplier_key
   , COALESCE(dim_supplier.supplier_name, "Undefined") AS supplier_name
 FROM dim_product__convert_boolean AS dim_product
