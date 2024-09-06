2c351ebd1c0b17273b0415f0a0cfde883b37809e
duynghia <voduyanqn1972@gmail.com>
2024-09-06 17:57:46 +0700

    update

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -20,6 +20,7 @@ WITH dim_product__source AS (
     , supplier_id AS supplier_key
     , color_id AS color_key
     , unit_package_id AS unit_package_types_key
+    , outer_package_id AS outer_package_types_key
   FROM dim_product__source
 )
 
@@ -39,6 +40,7 @@ WITH dim_product__source AS (
     , CAST(supplier_key AS INTEGER) AS supplier_key
     , CAST(color_key AS INTEGER) AS color_key
     , CAST(unit_package_types_key AS INTEGER) AS unit_package_types_key
+    , CAST(outer_package_types_key AS INTEGER) AS outer_package_types_key
   FROM dim_product__rename_column
 )
 
@@ -71,6 +73,7 @@ WITH dim_product__source AS (
     , COALESCE(supplier_key, 0) AS supplier_key
     , COALESCE(color_key, 0) AS color_key
     , COALESCE(unit_package_types_key, 0) AS unit_package_types_key
+    , COALESCE(outer_package_types_key, 0) AS outer_package_types_key
   FROM dim_product__convert_boolean
 )
 
@@ -90,6 +93,7 @@ WITH dim_product__source AS (
     , supplier_key
     , color_key
     , unit_package_types_key
+    , outer_package_types_key
   FROM dim_product__coalesce_record
 
   UNION ALL 
@@ -108,6 +112,7 @@ WITH dim_product__source AS (
     , 0 as supplier_key
     , 0 as color_key
     , 0 as unit_package_types_key
+    , 0 as outer_package_types_key
 
   UNION ALL
   SELECT
@@ -125,6 +130,7 @@ WITH dim_product__source AS (
     , -1 as supplier_key
     , -1 as color_key
     , -1 as unit_package_types_key
+    , -1 as outer_package_types_key
 )
 
 SELECT 
@@ -145,6 +151,8 @@ SELECT
   , COALESCE(dim_color.color_name, "Undefined") AS color_name
   , dim_product.unit_package_types_key
   , COALESCE(dim_unit_package_types.package_types_name, "Undefined") AS unit_package_types_name
+  , dim_product.outer_package_types_key
+  , COALESCE(dim_outer_package_types.package_types_name, "Undefined") AS outer_package_types_name
 FROM dim_product__undefine_column AS dim_product
 LEFT JOIN {{ ref('dim_supplier') }} AS dim_supplier
 ON dim_product.supplier_key = dim_supplier.supplier_key
@@ -152,3 +160,5 @@ LEFT JOIN {{ ref('stg_dim_color') }} AS dim_color
 ON dim_product.color_key = dim_color.color_key
 LEFT JOIN {{ ref('stg_dim_package_types') }} AS dim_unit_package_types
 ON dim_product.unit_package_types_key = dim_unit_package_types.package_types_key
+LEFT JOIN {{ ref('stg_dim_package_types') }} AS dim_outer_package_types
+ON dim_product.outer_package_types_key = dim_outer_package_types.package_types_key
